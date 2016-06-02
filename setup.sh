3fcf409a51dd99fa52159e88449bc7d5b45930a0
Chris Anderson <canderson30@unl.edu>
2016-06-02 12:35:25 -0500

    update names, setup, qc

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ fi
 # trimmomatic
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.33.zip
 unzip Trimmomatic-0.33.zip 
+mv Trimmomatic-0.33/trimmomatic-0.33.jar anaconda/envs/rumenVirome/bin/
 
 # cd-hit
 wget https://cdhit.googlecode.com/files/cd-hit-v4.6.1-2012-08-27.tgz
@@ -27,18 +28,24 @@ tar -xvf cd-hit-v4.6.1-2012-08-27.tgz
 cd cd-hit-v4.6.1-2012-08-27
 make openmp=yes
 cd ..
+mv cd-hit-v4.6.1-2012-08-27/cd-hit-454 anaconda/envs/rumenVirome/bin/
 
 # prinseq-lite
 wget http://sourceforge.net/projects/prinseq/files/standalone/prinseq-lite-0.20.4.tar.gz
 tar -xvf prinseq-lite-0.20.4.tar.gz
+mv prinseq-lite-0.20.4/prinseq-lite.pl anaconda/envs/rumenVirome/bin/
 
-# anaconda, R, QIIME
+# anaconda 
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 bash Anaconda-2.3.0-Linux-x86_64.sh
-anaconda/bin/conda create -n rumenVirome python=2.7 pip numpy=1.9.2 matplotlib=1.4.3 scipy=0.16.0 pandas=0.16.2 cython mock=1.1.3 nose=1.3.7
+anaconda/bin/conda create -n rumenVirome python=2.7 pip numpy=1.9.2 matplotlib=1.4.3 scipy=0.16.0 pandas=0.16.2 cython mock=1.1.3 nose=1.3.7 urllib beautifulsoup4
 source anaconda/bin/activate rumenVirome
+
+# R
 conda install -c r r=3.2.1
 conda install -c https://conda.binstar.org/r rpy2=2.5.6 r-devtools r-rcurl
+
+# QIIME
 pip install qiime==1.9.1
 
 # cutadapt
@@ -69,6 +76,7 @@ conda install -c https://conda.anaconda.org/asmeurer pandoc
 # spades
 wget http://spades.bioinf.spbau.ru/release3.5.0/SPAdes-3.5.0-Linux.tar.gz
 tar -zxvf SPAdes-3.5.0-Linux.tar.gz
+mv SPAdes-3.5.0-Linux/bin/* anaconda/envs/rumenVirome/bin/
 
 # prodigal
 wget https://github.com/hyattpd/Prodigal/archive/v2.6.2.zip
@@ -77,47 +85,38 @@ cur=$(pwd)
 cd Prodigal-2.6.2/
 make install INSTALLDIR=$cur
 cd ..
+mv prodigal anaconda/envs/rumenVirome/bin/
 
 # bowtie2
 wget http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.5/bowtie2-2.2.5-linux-x86_64.zip/download -O bowtie-2.2.5.source.zip
 unzip bowtie-2.2.5.source.zip
+mv bowtie2-2.2.5/bowtie2 anaconda/envs/rumenVirome/bin/
+mv bowtie2-2.2.5/bowtie2-build anaconda/envs/rumenVirome/bin/
 
 # R packages
 printf "\nInstallling R packages, will take some time...\n"
 R CMD BATCH scripts/install_pack.R
 
-# get Data
-wget ftp://129.93.221.145/public/raw_total.tgz
-wget ftp://129.93.221.145/public/raw_viral.tgz
-wget ftp://129.93.221.145/public/raw_illumina_viral_run1.tgz
-wget ftp://129.93.221.145/public/raw_illumina_viral_run2.tgz
-wget ftp://129.93.221.145/public/raw_illumina_viral_run3.tgz
-wget ftp://129.93.221.145/public/raw_illumina_viral_run4.tgz
-tar -zxvf raw_total.tgz
-tar -zxvf raw_viral.tgz
-tar -zxvf raw_illumina_viral_run1.tgz
-tar -zxvf raw_illumina_viral_run2.tgz
-tar -zxvf raw_illumina_viral_run3.tgz
-tar -zxvf raw_illumina_viral_run4.tgz
-
-# get intermediate_results directory
-wget -m ftp://129.93.221.145/public/intermediate_results
-mv 129.93.221.145/public/intermediate_results ./
-rm -rf 129.93.221.145/
-
-# remove
+# clean up
 rm cd-hit-v4.6.1-2012-08-27.tgz
+rm -rf cd-hit-v4.6.1-2012-08-27
 rm prinseq-lite-0.20.4.tar.gz
+rm -rf prinseq-lite-0.20.4
 rm Mothur.cen_64.zip
 rm -rf __MACOSX
 rm Trimmomatic-0.33.zip
 rm rRNA_prediction.tar
 rm Anaconda-2.3.0-Linux-x86_64.sh
-rm v2.6.2.zip
 rm SPAdes-3.5.0-Linux.tar.gz
-rm raw_viral.tgz
-rm raw_total.tgz
-rm raw_illumina_viral_run1.tgz
-rm raw_illumina_viral_run2.tgz
-rm raw_illumina_viral_run3.tgz
-rm raw_illumina_viral_run4.tgz
+rm -rf SPAdes-3.5.0-Linux
+rm bowtie-2.2.5.source.zip
+rm -rf bowtie2-2.2.5
+rm v2.6.2.zip
+rm -rf mothur
+rm -rf Trimmomatic-0.33
+rm -rf Prodigal-2.6.2
+rm install_pack.Rout
+mv transp_adapt_remove.cat.txt anaconda/envs/rumenVirome/bin/
+mv total_adapt_remove.cat.txt anaconda/envs/rumenVirome/bin/
+
+
